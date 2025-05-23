897d09d5093f2ba7d94cdc3ff3a537f61799ea5b
sweetycaty <sweetycaty@gmail.com>
2025-05-23 10:19:33 +0200

    Update officespace.py

--- officespace.py
+++ officespace.py
@@ -36,18 +36,6 @@ if "bookings" not in st.session_state:
     st.session_state.bookings = load_bookings_from_sheets()
 
 
-selected = st.session_state.bookings[key]
-try:
-    idx = team_members.index(selected)
-except ValueError:
-    idx = 0  # Default to empty
-st.selectbox(
-    label=desk_name,
-    options=team_members,
-    index=idx,
-    key=key,
-    label_visibility="visible"
-)
 
 # === Today for Scroll Logic ===
 today = datetime.today()
