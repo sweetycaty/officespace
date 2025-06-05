310140199c47d04d0ffb7a4878b691c75dedcb24
sweetycaty <sweetycaty@gmail.com>
2025-06-05 10:10:04 +0200

    Update officespace.py

--- officespace.py
+++ officespace.py
@@ -33,7 +33,7 @@ desk_labels = [
     "DB Desk",
     "RPM Desk"
 ]
-team_members = [" ", "BH", "BMc", "MM", "CVF", "EP", "DB", "AB"]
+team_members = [" ","FREE", "BH", "BMc", "MM", "CVF", "EP", "DB", "AB"]
 
 # Mapping of desk_labels to their default team member
 DEFAULT_DESK_ASSIGNMENTS = {
